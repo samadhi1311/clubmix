46b2cc0cb6e6c3360f0a78f08deed91fe5c34a1b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-04-19 22:50:05 +0530

    feat: Added simple testing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { MusicIcon } from 'lucide-react';
+import MixerInterface from './components/mixer-interface';
 import './App.css';
 
 function App() {
 	return (
-		<main>
-			<h1 className='text-3xl '>clubmix</h1>
-		</main>
+		<div className='min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-gray-100 p-4 md:p-6'>
+			<header className='mb-6'>
+				<div className='flex items-center gap-2'>
+					<MusicIcon className='text-purple-500' size={32} />
+					<h1 className='text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-600'>clubmix</h1>
+				</div>
+			</header>
+
+			<main className='container mx-auto'>
+				<MixerInterface />
+			</main>
+		</div>
 	);
 }
 
